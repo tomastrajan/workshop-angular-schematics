d422217ab2d19acceb7f206006d84fc1b56c5e1f
tomastrajan <tomas.trajan@gmail.com>
2019-07-03 13:44:08 +0200

    chore(workspace): add more readme files, add options completed stub

--- 2_schematics_options/hello_completed/src/hello/index.ts
+++ 2_schematics_options/hello_completed/src/hello/index.ts
@@ -1,14 +1,7 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-// TODO 1. build and run schematics with random parameters (eg --a --b --c 123)
+import { Schema } from './schema';
 
-// TODO 2. create schema.d.ts file in /hello folder
-
-// TODO 3. create and export typescript interface with name Schema which will contain name property of a type string
-
-// TODO 4. import and use Schema interface in this file to type _options argument of the hello function
-
-// TODO 5. create schema.json file in /hello folder
 
 // TODO 6. copy "initial" content into schema.json file from ./helpers folder
 
@@ -16,7 +9,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // TODO 8. reference schema.json in the collection.json file for hello schematics ( using  schema property and path to file )
 
-export function hello(_options: any): Rule {
+export function hello(_options: Schema): Rule {
 
   return (tree: Tree, _context: SchematicContext) => {
 
@@ -28,7 +21,7 @@ export function hello(_options: any): Rule {
 
     tree.create('hello.js', `console.log('Hello NG-MY!');`);
 
-    // TODO 10. build and run schematics with --name param to create file (mind --dry-run=false because of the dev mode)
+    // TODO 10. build and run schematics with --name param to create file (mind --dry-run because of the dev mode)
 
     // TODO 11. run created file using node
 
@@ -36,7 +29,7 @@ export function hello(_options: any): Rule {
 
     return tree;
 
-    // TODO 13. add new option in both schema.d.ts and schema.json ( for example greeting type, use string type and specify possible values in enum array property )
+    // TODO 13. add new option in both schema.d.ts and schema.json ( for example greeting type, use enum type and specify possible values )
 
     // TODO 14. add default value (eg Hello) and x-prompt for the new property
 
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Angular Shematics Workshop
+# Angular Schematics Workshop
 
+## Exercises
+
+1. [Schematics Basics](./1_schematics_basics/README.md)
+2. [Schematics Options](./2_schematics_options/README.md)
+3. [Schematics Templates](./3_schematics_templates/README.md)
 
 ## Useful commands
 
-* `schematics .:<schematic-name>` - run schematics with `schematics-name` from the current project (`.`), the `:` is a separator between the schematics package and scheamtics name
+* `schematics .:<schematic-name>` - run schematics with `schematics-name` from the current project (`.`), 
+   the `:` is a separator between the schematics package and schematics name
